1bf9a4a64bfb0608cbb84d930ba5a906e3e56a98
Jane Streeter <jane.america.streeter@gmail.com>
2024-01-11 09:02:29 -0600

    chore: added reference

--- .github/actions/branch-delete/main.js
+++ .github/actions/branch-delete/main.js
@@ -1,6 +1,7 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 const exec = require("@actions/exec");
+// refer to: https://www.github.com/actions/toolkit
 
 function run() {
   const env = exec.exec("env");
