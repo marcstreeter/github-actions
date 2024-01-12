398b2f16c41ca08cc9b140b911ae038920c5a816
Jane Streeter <jane.america.streeter@gmail.com>
2024-01-12 12:12:29 -0600

    chore: updated references per documentation and not chatgpt this time

--- branch-delete/main.js
+++ branch-delete/main.js
@@ -1,6 +1,5 @@
 const core = require("@actions/core");
-const { getOctokit } = require("@actions/github");
-const exec = require("@actions/exec");
+const github = require("@actions/github");
 // refer to: https://www.github.com/actions/toolkit
 
 async function run() {
@@ -8,10 +7,13 @@ async function run() {
   const inputRepo = core.getInput("repository", { required: true });
   const inputToken = core.getInput("auth_token", { required: true });
   const [repo_owner, repo_name] = inputRepo.split("/");
-  const octokit = getOctokit(inputToken);
+  const octokit = github.getOctokit(inputToken);
 
   try {
-    await octokit.git.deleteRef({
+    // per ref: https://github.com/actions/toolkit/blob/main/packages/github/README.md
+    // per ref: https://github.com/octokit/octokit.js?tab=readme-ov-file#octokitrequest
+    // per ref: https://github.com/octokit/octokit.js?tab=readme-ov-file#octokitrest-endpoint-methods (not using but may want to change it to this)
+    await octokit.request("DELETE /repos/{owner}/{repo}/git/refs/{ref}", {
       owner: repo_owner,
       repo: repo_name,
       ref: `heads/${inputBranch}`,
